2d86d56116c96db68ef62e910cd601820110cec0
Collin Choy <collin@ripplematch.com>
2020-08-10 03:27:54 +0000

    Add index page.

--- flask-web/flask_web/app.py
+++ flask-web/flask_web/app.py
@@ -1,9 +1,21 @@
-from flask import Flask
+from flask import Flask, request, render_template
 
 
 app = Flask(__name__)
+app.config["TEMPLATES_AUTO_RELOAD"] = True
 
 
 @app.route("/healthz")
 def health_check():
     return {"status": "up"}
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/submit", methods=["POST"])
+def submit():
+    print(request.get_json())
+    return {"prediction": {"guess": "Survived", "certainty": 0.8}}
